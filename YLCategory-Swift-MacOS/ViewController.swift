dde222cf11451a256476ae4cc89e36c2228d230b
魏宇龙 <weiyulong1987@163.com>
2024-12-07 17:12:12 +0800

    Initial Commit

---
+++ YLCategory-Swift-MacOS/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  YLCategory-Swift-MacOS
+//
+//  Created by 魏宇龙 on 2024/12/7.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
