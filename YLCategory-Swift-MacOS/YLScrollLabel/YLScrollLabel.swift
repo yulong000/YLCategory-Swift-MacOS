1c5fad943b1ca8ae9fd131fe250c69da3f07f5c5
魏宇龙 <weiyulong1987@163.com>
2025-03-04 11:02:18 +0800

    YLScrollLabel 优化

--- YLCategory-Swift-MacOS/YLScrollLabel/YLScrollLabel.swift
+++ YLCategory-Swift-MacOS/YLScrollLabel/YLScrollLabel.swift
@@ -5,29 +5,76 @@
 //  Created by 魏宇龙 on 2025/3/3.
 //
 
-import AppKit
+import Cocoa
 
-class YLScrollLabel: NSView {
+class YLScrollLabel: NSView, CAAnimationDelegate {
     
     /// 文本内容
-    var stringValue: String? = "" {
-        didSet {
-            showLabel.stringValue = stringValue ?? ""
-            scrollLabel.stringValue = stringValue ?? ""
+    var stringValue: String? {
+        set {
+            textLayer.string = newValue ?? ""
+            guard let stringValue = newValue else {
+                textSize = .zero
+                originFrame = .zero
+                scrollOriginFrame = .zero
+                canScroll = false
+                animationDuration = 0
+                textLayer.removeAllAnimations()
+                needsLayout = true
+                return
+            }
+            let maxSize: NSSize = NSSize(width: CGFloat.greatestFiniteMagnitude, height: CGFloat.greatestFiniteMagnitude)
+            let attrs: [NSAttributedString.Key: Any] = [.font : textLayer.font as! NSFont]
+            textSize = stringValue.boundingRect(with: maxSize, options: [.usesFontLeading, .usesLineFragmentOrigin], attributes: attrs).size
+            textSize = NSSize(width: ceil(textSize.width), height: ceil(textSize.height))
             needsLayout = true
         }
-    }
-    /// 文字颜色
-    var textColor: NSColor? {
-        didSet {
-            showLabel.textColor = textColor
-            scrollLabel.textColor = textColor
+        get {
+            return textLayer.string as? String
         }
     }
+    
+    /// 字体
+    var font: NSFont {
+        set {
+            textLayer.font = newValue
+            textLayer.fontSize = newValue.pointSize
+            stringValue = stringValue
+        }
+        get {
+            return textLayer.font as! NSFont
+        }
+    }
+    
+    /// 文字颜色
+    var textColor: NSColor? {
+        set {
+            textLayer.foregroundColor = newValue?.cgColor
+        }
+        get {
+            guard let cgColor = textLayer.foregroundColor else { return nil }
+            return NSColor(cgColor: cgColor)
+        }
+    }
+
     /// 显示模式
-    var lineBreakMode: NSLineBreakMode = .byTruncatingMiddle {
-        didSet {
-            showLabel.lineBreakMode = lineBreakMode
+    var lineBreakMode: NSLineBreakMode {
+        set {
+            switch newValue {
+            case .byTruncatingHead:     textLayer.truncationMode = .start
+            case .byTruncatingTail:     textLayer.truncationMode = .end
+            case .byTruncatingMiddle:   textLayer.truncationMode = .middle
+            default: break
+            }
+        }
+        get {
+            switch textLayer.truncationMode {
+            case .start:    return .byTruncatingHead
+            case .end:      return .byTruncatingTail
+            case .middle:   return .byTruncatingMiddle
+            default: break
+            }
+            return .byTruncatingMiddle
         }
     }
     // 是否开启滚动
@@ -46,22 +93,30 @@ class YLScrollLabel: NSView {
     }
     
     private func initialize() {
-        addSubview(scrollView)
+        wantsLayer = true
+        layer?.addSublayer(textLayer)
+        layer?.masksToBounds = true
     }
     
     override func layout() {
         super.layout()
-        scrollLabel.sizeToFit()
-        showLabel.sizeToFit()
+        
+        guard let _ = stringValue else {
+            textLayer.frame = .zero
+            trackingAreas.forEach { removeTrackingArea($0) }
+            return
+        }
+
         let width = bounds.size.width
         let height = bounds.size.height
-        let textWidth = scrollLabel.bounds.size.width
-        let textHeight = scrollLabel.bounds.size.height
+        let y: CGFloat = (height - textSize.height) / 2
+        textLayer.frame = NSRect(x: 0, y: y, width: width, height: textSize.height)
         
-        scrollView.frame = NSRect(x: 0, y: (height - textHeight) / 2, width: width, height: textHeight)
-        showLabel.frame = NSRect(x: 0, y: 0, width: width, height: showLabel.bounds.size.height)
+        scrollOriginFrame = NSRect(x: 0, y: y, width: textSize.width, height: textSize.height)
+        originFrame = textLayer.frame
         
-        canScroll = textWidth > width
+        canScroll = textSize.width > width
+        animationDuration = (textSize.width - width) / 50
         
         // 监听鼠标划入｜划出
         addMouseTrackingArea()
@@ -69,9 +124,11 @@ class YLScrollLabel: NSView {
     
     // MARK: - 鼠标手势
     
-    private var canScroll = false       // 是否可以滚动
-    private var timer: Timer?           // 定时器
-    private var reverseScroll = false   // 反方向滚动
+    private var canScroll = false                   // 是否可以滚动
+    private var textSize: NSSize = .zero            // 文本的大小
+    private var animationDuration: CGFloat = 0.0    // 动画需要的时长
+    private var scrollOriginFrame: NSRect = .zero   // 滚动时，最初的frame
+    private var originFrame: NSRect = .zero         // 静止时的frame
     
     private func addMouseTrackingArea() {
         trackingAreas.forEach { removeTrackingArea($0) }
@@ -82,21 +139,16 @@ class YLScrollLabel: NSView {
     override func mouseEntered(with event: NSEvent) {
         super.mouseEntered(with: event)
         if !canScroll || !isScrollEnable { return }
-        if timer == nil {
-            scrollView.documentView = scrollLabel
-            timer = Timer(timeInterval: 0.02, target: self, selector: #selector(autoScroll), userInfo: nil, repeats: true)
-            RunLoop.main.add(timer!, forMode: .default)
-        }
+        textLayer.frame = scrollOriginFrame
+        textLayer.removeAllAnimations()
+        textLayer.add(createScrollAnimation(), forKey: "scroll")
     }
-    
+
     override func mouseExited(with event: NSEvent) {
         super.mouseExited(with: event)
         if !canScroll || !isScrollEnable { return }
-        timer?.invalidate()
-        timer = nil
-        scrollView.documentView = showLabel
-        scrollView.contentView.scroll(to: .zero)
-        reverseScroll = false
+        textLayer.frame = originFrame
+        textLayer.removeAllAnimations()
     }
     
     override func mouseDown(with event: NSEvent) {
@@ -104,45 +156,59 @@ class YLScrollLabel: NSView {
         clickHandler?(self)
     }
     
-    @objc func autoScroll() {
-        var visibleRect = scrollView.documentVisibleRect
-        if reverseScroll {
-            visibleRect.origin.x -= 1
-        } else {
-            visibleRect.origin.x += 1
+    func animationDidStop(_ anim: CAAnimation, finished flag: Bool) {
+        if !flag {
+            textLayer.frame = originFrame
+            textLayer.removeAllAnimations()
+            return
         }
-        if NSMaxX(visibleRect) >= scrollLabel.bounds.size.width {
-            reverseScroll = true
-            timer?.fireDate = Date(timeIntervalSinceNow: 0.5)
-        } else if visibleRect.origin.x == 0 {
-            reverseScroll = false
-            timer?.fireDate = Date(timeIntervalSinceNow: 0.5)
+        if let scrollAnimation = textLayer.animation(forKey: "scroll"),
+           scrollAnimation == anim {
+            // 正向滚动结束，添加方向滚动
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                self.textLayer.add(self.createReverseScrollAnimation(), forKey: "reverseScroll")
+            }
+            return
         }
-        scrollView.contentView.scroll(to: visibleRect.origin)
+        if let reverseScrollAnimation = textLayer.animation(forKey: "reverseScroll"),
+           reverseScrollAnimation == anim {
+            // 反向滚动结束，添加正向滚动
+            textLayer.removeAllAnimations()
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                self.textLayer.add(self.createScrollAnimation(), forKey: "scroll")
+            }
+            return
+        }
     }
     
-    // MARK: - UI
+    private func createScrollAnimation() -> CABasicAnimation {
+        let animation = CABasicAnimation(keyPath: "transform.translation.x")
+        animation.duration = animationDuration
+        animation.isRemovedOnCompletion = false
+        animation.fillMode = .forwards
+        animation.timingFunction = CAMediaTimingFunction(name: .linear)
+        animation.toValue = bounds.width - textLayer.bounds.width
+        animation.delegate = self
+        return animation
+    }
+    private func createReverseScrollAnimation() -> CABasicAnimation {
+        let animation = CABasicAnimation(keyPath: "transform.translation.x")
+        animation.duration = animationDuration
+        animation.isRemovedOnCompletion = false
+        animation.fillMode = .forwards
+        animation.timingFunction = CAMediaTimingFunction(name: .linear)
+        animation.toValue = 0
+        animation.delegate = self
+        return animation
+    }
     
-    private lazy var scrollView: ScrollView = {
-        let scrollView = ScrollView()
-        scrollView.hasVerticalScroller = false
-        scrollView.hasHorizontalScroller = false
-        scrollView.documentView = showLabel
-        scrollView.drawsBackground = false
-        return scrollView
-    }()
-    private lazy var scrollLabel: NSTextField = {
-        let textLabel = NSTextField(labelWithString: "")
-        return textLabel
-    }()
-    private lazy var showLabel: NSTextField = {
-        let textLabel = NSTextField(labelWithString: "")
-        textLabel.lineBreakMode = .byTruncatingMiddle
-        return textLabel
+    private lazy var textLayer: CATextLayer = {
+        let layer = CATextLayer()
+        layer.truncationMode = .middle
+        layer.isWrapped = false
+        layer.font = NSFont.systemFont(ofSize: 13)
+        layer.fontSize = 13
+        layer.contentsScale = NSScreen.main?.backingScaleFactor ?? 1
+        return layer
     }()
 }
-
-fileprivate class ScrollView: NSScrollView {
-    override func scrollWheel(with event: NSEvent) { }
-}
-
