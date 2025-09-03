b77e3a393d68606dcb50da2e70a0864e873b64f8
魏宇龙 <weiyulong1987@163.com>
2025-09-03 15:56:49 +0800

    增加  NSTextField 忽略鼠标点击事件

--- YLCategory-Swift-MacOS/NSTextField/NSTextField+Category.swift
+++ YLCategory-Swift-MacOS/NSTextField/NSTextField+Category.swift
@@ -8,11 +8,13 @@
 import Foundation
 import AppKit
 
-public extension NSTextField {
+fileprivate var IgnoresMouseEventsKey: Bool = false
+
+extension NSTextField {
     
     // MARK: 固定最大宽度，高度自适应
     @discardableResult
-    func sizeWith(maxWidth: CGFloat) -> NSSize {
+    public func sizeWith(maxWidth: CGFloat) -> NSSize {
         let size = sizeThatFits(NSMakeSize(maxWidth, CGFloat.greatestFiniteMagnitude))
         var frame = self.frame
         frame.size = size
@@ -22,11 +24,25 @@ public extension NSTextField {
     
     // MARK: 固定最大宽度，高度自适应
     @discardableResult
-    func sizeToFit(maxWidth: CGFloat) -> Self {
+    public func sizeToFit(maxWidth: CGFloat) -> Self {
         let size = sizeThatFits(NSMakeSize(maxWidth, CGFloat.greatestFiniteMagnitude))
         var frame = self.frame
         frame.size = size
         self.frame = frame
         return self
+    }
+    
+    // MARK: 忽略鼠标点击事件
+    @IBInspectable
+    open var ignoresMouseEvents: Bool {
+        get { return objc_getAssociatedObject(self, &IgnoresMouseEventsKey) as? Bool ?? false }
+        set { objc_setAssociatedObject(self, &IgnoresMouseEventsKey, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC) }
+    }
+    
+    open override func hitTest(_ point: NSPoint) -> NSView? {
+        if ignoresMouseEvents {
+            return nil
+        }
+        return super.hitTest(point)
     }
 }
