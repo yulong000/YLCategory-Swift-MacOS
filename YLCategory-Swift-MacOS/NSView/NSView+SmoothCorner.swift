a3eabe4c856715e868c2d5e57afefb99b959e6d0
魏宇龙 <weiyulong1987@163.com>
2025-09-05 18:23:15 +0800

    优化

--- YLCategory-Swift-MacOS/NSView/NSView+SmoothCorner.swift
+++ YLCategory-Swift-MacOS/NSView/NSView+SmoothCorner.swift
@@ -92,46 +92,45 @@ public extension NSView {
     
     // MARK: - Method Swizzling
     
-    /// 开启自动绘制平滑圆角，用自定义的draw(_:)方法替换原来的方法
+    /// 开启自动绘制平滑圆角
+    /// 用自定义的draw(_:) layout() 方法替换原来的方法
     static func enableSmoothCornerSwizzling() {
-        _ = self.swizzleDrawImplementation
+        _ = self.swizzleImplementation
     }
     
-    private static let swizzleDrawImplementation: Void = {
-        swizzleDraw()
+    /// 可以保证替换方法只执行一次
+    private static let swizzleImplementation: Void = {
+        swizzleDrawAndLayout()
     }()
     
-    private static func swizzleDraw() {
-        let cls = NSView.self
-        let originalSelector = #selector(draw(_:))
-        let swizzledSelector = #selector(swizzled_draw(_:))
+    /// 交换
+    private static func swizzleDrawAndLayout() {
         
-        guard let originalMethod = class_getInstanceMethod(cls, originalSelector),
-              let swizzledMethod = class_getInstanceMethod(cls, swizzledSelector) else {
+        // 交换 draw(_:)
+        guard let originalMethod = class_getInstanceMethod(NSView.self, #selector(draw(_:))),
+              let swizzledMethod = class_getInstanceMethod(NSView.self, #selector(swizzled_draw(_:))) else {
             return
         }
+        method_exchangeImplementations(originalMethod, swizzledMethod)
         
-        let didAddMethod = class_addMethod(cls,
-                                           originalSelector,
-                                           method_getImplementation(swizzledMethod),
-                                           method_getTypeEncoding(swizzledMethod))
-        
-        if didAddMethod {
-            class_replaceMethod(cls,
-                                swizzledSelector,
-                                method_getImplementation(originalMethod),
-                                method_getTypeEncoding(originalMethod))
-        } else {
-            method_exchangeImplementations(originalMethod, swizzledMethod)
+        // 交换 layout()
+        guard let originalMethod = class_getInstanceMethod(NSView.self, #selector(layout)),
+              let swizzledMethod = class_getInstanceMethod(NSView.self, #selector(swizzled_layout)) else {
+            return
         }
+        method_exchangeImplementations(originalMethod, swizzledMethod)
     }
     
     @objc private func swizzled_draw(_ dirtyRect: NSRect) {
-        // Call original draw(_:) (which is now swizzled_draw due to method_exchangeImplementations)
         swizzled_draw(dirtyRect)
-        // Then draw smooth corner
         drawSmoothCorner()
     }
+    
+    @objc private func swizzled_layout() {
+        swizzled_layout()
+        drawSmoothCorner()
+    }
+
 }
 
 
@@ -172,100 +171,98 @@ open class NSViewCornerBezierPath: NSBezierPath {
     convenience init(rect: NSRect, corner: NSViewMaskCorner) {
         self.init()
         
-        let newCorner = adjust(mask: corner, with: rect)
-        
         var last = NSPoint(x: rect.width, y: rect.origin.y)
-        move(to: NSPoint(x: rect.origin.x + newCorner.topLeft * coeff, y: last.y))
+        move(to: NSPoint(x: rect.origin.x + corner.topLeft * coeff, y: last.y))
         
         // top
         
-        line(to: NSPoint(x: last.x - newCorner.topRight * coeff, y: last.y))
+        line(to: NSPoint(x: last.x - corner.topRight * coeff, y: last.y))
         
         // top right c1
-        last = NSPoint(x: last.x - newCorner.topRight * coeff, y: last.y)
-        curve(to: NSPoint(x: last.x + newCorner.topRight * 0.77037, y: last.y + newCorner.topRight * 0.13357),
-              controlPoint1: NSPoint(x: last.x + newCorner.topRight * 0.44576, y: last.y),
-              controlPoint2: NSPoint(x: last.x + newCorner.topRight * 0.6074, y: last.y + newCorner.topRight * 0.04641))
+        last = NSPoint(x: last.x - corner.topRight * coeff, y: last.y)
+        curve(to: NSPoint(x: last.x + corner.topRight * 0.77037, y: last.y + corner.topRight * 0.13357),
+              controlPoint1: NSPoint(x: last.x + corner.topRight * 0.44576, y: last.y),
+              controlPoint2: NSPoint(x: last.x + corner.topRight * 0.6074, y: last.y + corner.topRight * 0.04641))
         
         // top right c2
-        last = NSPoint(x: last.x + newCorner.topRight * 0.77037, y: last.y + newCorner.topRight * 0.13357)
-        curve(to: NSPoint(x: last.x + newCorner.topRight * 0.37801, y: last.y + newCorner.topRight * 0.37801),
-              controlPoint1: NSPoint(x: last.x + newCorner.topRight * 0.16296, y: last.y + newCorner.topRight * 0.08715),
-              controlPoint2: NSPoint(x: last.x + newCorner.topRight * 0.290086, y: last.y + newCorner.topRight * 0.2150))
+        last = NSPoint(x: last.x + corner.topRight * 0.77037, y: last.y + corner.topRight * 0.13357)
+        curve(to: NSPoint(x: last.x + corner.topRight * 0.37801, y: last.y + corner.topRight * 0.37801),
+              controlPoint1: NSPoint(x: last.x + corner.topRight * 0.16296, y: last.y + corner.topRight * 0.08715),
+              controlPoint2: NSPoint(x: last.x + corner.topRight * 0.290086, y: last.y + corner.topRight * 0.2150))
         
         // top right c3
-        last = NSPoint(x: last.x + newCorner.topRight * 0.37801, y: last.y + newCorner.topRight * 0.37801)
-        curve(to: NSPoint(x: last.x + newCorner.topRight * 0.13357, y: last.y + newCorner.topRight * 0.77037),
-              controlPoint1: NSPoint(x: last.x + newCorner.topRight * 0.08715, y: last.y + newCorner.topRight * 0.16296),
-              controlPoint2: NSPoint(x: last.x + newCorner.topRight * 0.13357, y: last.y + newCorner.topRight * 0.32461))
+        last = NSPoint(x: last.x + corner.topRight * 0.37801, y: last.y + corner.topRight * 0.37801)
+        curve(to: NSPoint(x: last.x + corner.topRight * 0.13357, y: last.y + corner.topRight * 0.77037),
+              controlPoint1: NSPoint(x: last.x + corner.topRight * 0.08715, y: last.y + corner.topRight * 0.16296),
+              controlPoint2: NSPoint(x: last.x + corner.topRight * 0.13357, y: last.y + corner.topRight * 0.32461))
         
         // right
         
         last = NSPoint(x: rect.size.width, y: rect.size.height)
-        line(to: NSPoint(x: last.x, y: last.y - newCorner.bottomRight * coeff))
+        line(to: NSPoint(x: last.x, y: last.y - corner.bottomRight * coeff))
         
         // bottom right c1
-        last = NSPoint(x: last.x, y: last.y - newCorner.bottomRight * coeff)
-        curve(to: NSPoint(x: last.x - newCorner.bottomRight * 0.13357, y: last.y + newCorner.bottomRight * 0.77037),
-              controlPoint1: NSPoint(x: last.x, y: last.y + newCorner.bottomRight * 0.44576),
-              controlPoint2: NSPoint(x: last.x - newCorner.bottomRight * 0.04641, y: last.y + newCorner.bottomRight * 0.6074))
+        last = NSPoint(x: last.x, y: last.y - corner.bottomRight * coeff)
+        curve(to: NSPoint(x: last.x - corner.bottomRight * 0.13357, y: last.y + corner.bottomRight * 0.77037),
+              controlPoint1: NSPoint(x: last.x, y: last.y + corner.bottomRight * 0.44576),
+              controlPoint2: NSPoint(x: last.x - corner.bottomRight * 0.04641, y: last.y + corner.bottomRight * 0.6074))
         
         // bottom right c2
-        last = NSPoint(x: last.x - newCorner.bottomRight * 0.13357, y: last.y + newCorner.bottomRight * 0.77037)
-        curve(to: NSPoint(x: last.x - newCorner.bottomRight * 0.37801, y: last.y + newCorner.bottomRight * 0.37801),
-              controlPoint1: NSPoint(x: last.x - newCorner.bottomRight * 0.08715, y: last.y + newCorner.bottomRight * 0.16296),
-              controlPoint2: NSPoint(x: last.x - newCorner.bottomRight * 0.21505, y: last.y + newCorner.bottomRight * 0.290086))
+        last = NSPoint(x: last.x - corner.bottomRight * 0.13357, y: last.y + corner.bottomRight * 0.77037)
+        curve(to: NSPoint(x: last.x - corner.bottomRight * 0.37801, y: last.y + corner.bottomRight * 0.37801),
+              controlPoint1: NSPoint(x: last.x - corner.bottomRight * 0.08715, y: last.y + corner.bottomRight * 0.16296),
+              controlPoint2: NSPoint(x: last.x - corner.bottomRight * 0.21505, y: last.y + corner.bottomRight * 0.290086))
         
         // bottom right c3
-        last = NSPoint(x: last.x - newCorner.bottomRight * 0.37801, y: last.y + newCorner.bottomRight * 0.37801)
-        curve(to: NSPoint(x: last.x - newCorner.bottomRight * 0.77037, y: last.y + newCorner.bottomRight * 0.13357),
-              controlPoint1: NSPoint(x: last.x - newCorner.bottomRight * 0.16296, y: last.y + newCorner.bottomRight * 0.08715),
-              controlPoint2: NSPoint(x: last.x - newCorner.bottomRight * 0.32461, y: last.y + newCorner.bottomRight * 0.13357))
+        last = NSPoint(x: last.x - corner.bottomRight * 0.37801, y: last.y + corner.bottomRight * 0.37801)
+        curve(to: NSPoint(x: last.x - corner.bottomRight * 0.77037, y: last.y + corner.bottomRight * 0.13357),
+              controlPoint1: NSPoint(x: last.x - corner.bottomRight * 0.16296, y: last.y + corner.bottomRight * 0.08715),
+              controlPoint2: NSPoint(x: last.x - corner.bottomRight * 0.32461, y: last.y + corner.bottomRight * 0.13357))
         
         // bottom
         
         last = NSPoint(x: rect.origin.x, y: rect.height)
-        line(to: NSPoint(x: last.x + newCorner.bottomLeft * coeff, y: last.y))
+        line(to: NSPoint(x: last.x + corner.bottomLeft * coeff, y: last.y))
         
         // bottom left c1
-        last = NSPoint(x: last.x + newCorner.bottomLeft * coeff, y: last.y)
-        curve(to: NSPoint(x: last.x - newCorner.bottomLeft * 0.77037, y: last.y - newCorner.bottomLeft * 0.13357),
-              controlPoint1: NSPoint(x: last.x - newCorner.bottomLeft * 0.44576, y: last.y),
-              controlPoint2: NSPoint(x: last.x - newCorner.bottomLeft * 0.6074, y: last.y - newCorner.bottomLeft * 0.04641))
+        last = NSPoint(x: last.x + corner.bottomLeft * coeff, y: last.y)
+        curve(to: NSPoint(x: last.x - corner.bottomLeft * 0.77037, y: last.y - corner.bottomLeft * 0.13357),
+              controlPoint1: NSPoint(x: last.x - corner.bottomLeft * 0.44576, y: last.y),
+              controlPoint2: NSPoint(x: last.x - corner.bottomLeft * 0.6074, y: last.y - corner.bottomLeft * 0.04641))
         
         // bottom left c2
-        last = NSPoint(x: last.x - newCorner.bottomLeft * 0.77037, y: last.y - newCorner.bottomLeft * 0.13357)
-        curve(to: NSPoint(x: last.x - newCorner.bottomLeft * 0.37801, y: last.y - newCorner.bottomLeft * 0.37801),
-              controlPoint1: NSPoint(x: last.x - newCorner.bottomLeft * 0.16296, y: last.y - newCorner.bottomLeft * 0.08715),
-              controlPoint2: NSPoint(x: last.x - newCorner.bottomLeft * 0.290086, y: last.y - newCorner.bottomLeft * 0.2150))
+        last = NSPoint(x: last.x - corner.bottomLeft * 0.77037, y: last.y - corner.bottomLeft * 0.13357)
+        curve(to: NSPoint(x: last.x - corner.bottomLeft * 0.37801, y: last.y - corner.bottomLeft * 0.37801),
+              controlPoint1: NSPoint(x: last.x - corner.bottomLeft * 0.16296, y: last.y - corner.bottomLeft * 0.08715),
+              controlPoint2: NSPoint(x: last.x - corner.bottomLeft * 0.290086, y: last.y - corner.bottomLeft * 0.2150))
         
         // bottom left c3
-        last = NSPoint(x: last.x - newCorner.bottomLeft * 0.37801, y: last.y - newCorner.bottomLeft * 0.37801)
-        curve(to: NSPoint(x: last.x - newCorner.bottomLeft * 0.13357, y: last.y - newCorner.bottomLeft * 0.77037),
-              controlPoint1: NSPoint(x: last.x - newCorner.bottomLeft * 0.08715, y: last.y - newCorner.bottomLeft * 0.16296),
-              controlPoint2: NSPoint(x: last.x - newCorner.bottomLeft * 0.13357, y: last.y - newCorner.bottomLeft * 0.32461))
+        last = NSPoint(x: last.x - corner.bottomLeft * 0.37801, y: last.y - corner.bottomLeft * 0.37801)
+        curve(to: NSPoint(x: last.x - corner.bottomLeft * 0.13357, y: last.y - corner.bottomLeft * 0.77037),
+              controlPoint1: NSPoint(x: last.x - corner.bottomLeft * 0.08715, y: last.y - corner.bottomLeft * 0.16296),
+              controlPoint2: NSPoint(x: last.x - corner.bottomLeft * 0.13357, y: last.y - corner.bottomLeft * 0.32461))
         
         // left
         
-        line(to: NSPoint(x: rect.origin.x, y: rect.origin.y + newCorner.topLeft * coeff))
+        line(to: NSPoint(x: rect.origin.x, y: rect.origin.y + corner.topLeft * coeff))
         
         // top left c1
-        last = NSPoint(x: rect.origin.x, y: rect.origin.y + newCorner.topLeft * coeff)
-        curve(to: NSPoint(x: last.x + newCorner.topLeft * 0.13357, y: last.y - newCorner.topLeft * 0.77037),
-              controlPoint1: NSPoint(x: last.x, y: last.y - newCorner.topLeft * 0.44576),
-              controlPoint2: NSPoint(x: last.x + newCorner.topLeft * 0.04641, y: last.y - newCorner.topLeft * 0.6074))
+        last = NSPoint(x: rect.origin.x, y: rect.origin.y + corner.topLeft * coeff)
+        curve(to: NSPoint(x: last.x + corner.topLeft * 0.13357, y: last.y - corner.topLeft * 0.77037),
+              controlPoint1: NSPoint(x: last.x, y: last.y - corner.topLeft * 0.44576),
+              controlPoint2: NSPoint(x: last.x + corner.topLeft * 0.04641, y: last.y - corner.topLeft * 0.6074))
         
         // top left c2
-        last = NSPoint(x: last.x + newCorner.topLeft * 0.13357, y: last.y - newCorner.topLeft * 0.77037)
-        curve(to: NSPoint(x: last.x + newCorner.topLeft * 0.37801, y: last.y - newCorner.topLeft * 0.37801),
-              controlPoint1: NSPoint(x: last.x + newCorner.topLeft * 0.08715, y: last.y - newCorner.topLeft * 0.16296),
-              controlPoint2: NSPoint(x: last.x + newCorner.topLeft * 0.21505, y: last.y - newCorner.topLeft * 0.290086))
+        last = NSPoint(x: last.x + corner.topLeft * 0.13357, y: last.y - corner.topLeft * 0.77037)
+        curve(to: NSPoint(x: last.x + corner.topLeft * 0.37801, y: last.y - corner.topLeft * 0.37801),
+              controlPoint1: NSPoint(x: last.x + corner.topLeft * 0.08715, y: last.y - corner.topLeft * 0.16296),
+              controlPoint2: NSPoint(x: last.x + corner.topLeft * 0.21505, y: last.y - corner.topLeft * 0.290086))
         
         // top left c3
-        last = NSPoint(x: last.x + newCorner.topLeft * 0.37801, y: last.y - newCorner.topLeft * 0.37801)
-        curve(to: NSPoint(x: last.x + newCorner.topLeft * 0.77037, y: last.y - newCorner.topLeft * 0.13357),
-              controlPoint1: NSPoint(x: last.x + newCorner.topLeft * 0.16296, y: last.y - newCorner.topLeft * 0.08715),
-              controlPoint2: NSPoint(x: last.x + newCorner.topLeft * 0.32461, y: last.y - newCorner.topLeft * 0.13357))
+        last = NSPoint(x: last.x + corner.topLeft * 0.37801, y: last.y - corner.topLeft * 0.37801)
+        curve(to: NSPoint(x: last.x + corner.topLeft * 0.77037, y: last.y - corner.topLeft * 0.13357),
+              controlPoint1: NSPoint(x: last.x + corner.topLeft * 0.16296, y: last.y - corner.topLeft * 0.08715),
+              controlPoint2: NSPoint(x: last.x + corner.topLeft * 0.32461, y: last.y - corner.topLeft * 0.13357))
         
         close()
     }
@@ -293,40 +290,5 @@ open class NSViewCornerBezierPath: NSBezierPath {
         
         return path
     }
-    
-    private func adjust(mask corner: NSViewMaskCorner, with rect: NSRect) -> NSViewMaskCorner {
-        let w = CGFloat(rect.width)
-        let h = CGFloat(rect.height)
-        
-        let topLeft = corner.topLeft * coeff
-        let topRight = corner.topRight * coeff
-        let bottomRight = corner.bottomRight * coeff
-        let bottomLeft = corner.bottomLeft * coeff
-        
-        var shorter: CGFloat = 0
-        var x: CGFloat = 0
-        var newCorner = NSViewMaskCorner()
-        
-        // 右上
-        shorter = min(w, h)
-        x = topRight > shorter ? shorter : topRight
-        newCorner.topRight = x / coeff
-        
-        // 右下
-        shorter = min(w, h - x)
-        x = bottomRight > shorter ? shorter : bottomRight
-        newCorner.bottomRight = x / coeff
-        
-        // 左下
-        shorter = min(w - x, h)
-        x = bottomLeft > shorter ? shorter : bottomLeft
-        newCorner.bottomLeft = x / coeff
-        
-        // 左上
-        shorter = min(w - newCorner.topRight * coeff, h - x)
-        x = topLeft > shorter ? shorter : topLeft
-        newCorner.topLeft = x / coeff
-        
-        return newCorner
-    }
+
 }
